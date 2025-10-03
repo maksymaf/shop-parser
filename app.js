4cf17cbea0741f8df837708520c6bd80ae15a71a
maksymaf <forbidden344@gmail.com>
2025-10-03 21:40:16 +0300

    product parsing has been added

--- app.js
+++ app.js
@@ -8,10 +8,36 @@ const PORT = 3000;
 const app = express();
 
 app.use(cors({origin: `http://localhost:${PORT}`}));
+app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     return res.status(200).sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.post('/api/scrape', async (req, res) => {
+    const { url } = req.body;
+    try{
+
+        const response = await axios.get(url);
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        const productTitle = $('h1._3Trjq').text();
+        const productLogo = $('img.gCAIM').attr('src');
+        let productPrice = $('div.tqUsL div.bkjEo span.yzKb6').text();
+        
+        productPrice = parseInt(productPrice.split(' ').join(''));
+
+        return res.status(200).json({
+            productTitle,
+            productLogo,
+            productPrice
+        });
+
+    }catch(error){
+        return res.status(500).json({message: error.message});
+    }
 });
 
 app.listen(PORT, () => {
